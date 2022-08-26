473d4ce0362cbe91b4c278613632f9a63f7ceea9
Toby Chen <chenyanjun912@hotmail.com>
2022-08-26 01:51:15 -0700

    Disk

---
+++ prepare-disk-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+img=test.img
+
+dd if=/dev/zero of=$img bs=1MB count=512
+
+device=`sudo losetup -f --show $img`
+
+sudo gdisk $device <<EOF
+n
+1
+2048
++64M
+EF00
+n
+2
+
+
+8300
+w
+Y
+EOF
+sudo partprobe
+
+
+sudo mkfs.vfat ${device}p1
+sudo mkfs.ext4 ${device}p2
+
+sudo mkdir /opt/rootfs
+sudo mount ${device}p2 /opt/rootfs
