d2510332fdaf4bb4db4b388e6e6e3541c998b35a
Toby Chen <chenyanjun912@hotmail.com>
2022-08-30 15:05:23 -0700

    Update thirdstage.sh

--- thirdstage.sh
+++ thirdstage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 echo Enter thirdstage
 
-apt-get clean
-apt-get update
-
 # D.3.4.3. Setting Timezone
 # dpkg-reconfigure tzdata
 
 # D.3.4.4. Configure Networking
-# echo DebianHostName > /etc/hostname
+echo build-image > /etc/hostname
 
 # D.3.4.5. Configure Apt
+dist_release=`cat /etc/apt/sources.list | awk '{print $3}'`
+sed -e "h; p; s/${dist_release}/${dist_release}-security/; p; g; s/${dist_release}/${dist_release}-updates/" /etc/apt/sources.list | sed -e 's/main/main restricted universe multiverse/' | tee /tmp/sources.list
+cp /tmp/sources.list /etc/apt/sources.list
+rm /tmp/sources.list
+
+apt-get update
+apt-get upgrade
+apt-get clean
 
 # D.3.4.6. Configure Locales and Keyboard
 # apt-get install locales
@@ -21,12 +26,12 @@ apt-get install -y linux-image-generic || exit 1
 
 # D.3.6. Set up the Boot Loader
 apt-get install -y grub-efi-arm64 || exit 1
-# grub-install --removable || exit 1
-# update-grub || exit 1
+grub-install --removable || exit 1
+update-grub || exit 1
 
 # D.3.7. Remote access: Installing SSH and setting up access
 apt-get install -y ssh || exit 1
-adduser linux
+useradd -m -s /bin/bash linux
 echo Setting default passwords
 echo root:password | chpasswd
 echo linux:password | chpasswd
